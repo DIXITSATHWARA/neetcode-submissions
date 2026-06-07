ee16af2a77975c6641efbb3241e00ced138bf515
DIXITSATHWARA <34939454+DIXITSATHWARA@users.noreply.github.com>
2026-06-07 15:33:42 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+
+        const result = [];
+
+        for(let i = 0; i < nums.length; i++){
+
+            let mul = 1;
+
+            for(let j = 0; j < nums.length; j++){
+
+                if(i != j){
+                    mul *= nums[j];
+                }
+            }
+
+            result.push(mul);
+            
+        }
+
+        return result;
+    }
+}
